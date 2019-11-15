01114cb7760736c4480e112c77d729f0a0f7c0a5
Zakir Sajib <zakirsajib@gmail.com>
2019-11-15 14:45:14 +0000

    Add Google Analytics Code

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-152596464-1`,
       },
     },
     `gatsby-plugin-feed`,
